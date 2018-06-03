809ac2bab11743da1d88e8c5d5720dd5ba99f931
xwp <xwp_fullstack@163.com>
2018-06-03 09:30:24 +0800

    解决运行问题,并完善类框架

--- config.py
+++ config.py
@@ -1,2 +1,28 @@
+from redis import StrictRedis
 class Config(object):
+    DEBUG = None
+    # 设置密钥
+    SECRET_KEY = 'xjXwFyIzzcspKMfeOSCJcep4m7yaWt6P6iqrrtw+0FM='
+    # 链接数据库
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@127.0.0.1:3306/test_01'
+    # 设置数据库动态追踪修改
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # 配置redis信息
+    REDIS_HOST = '127.0.0.1'
+    REDIS_PORT = 6379
+    SESSION_TYPE = 'redis'
+    SESSION_RDIES = StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
+    SESSION_USE_SIGNER = True
+    PERMANENT_SESSION_LIFETIME = 86400
+
+class development_model(Config):
     DEBUG = True
+
+class production_model(Config):
+    DEBUG = False
+
+config = {
+
+    'development':development_model,
+    'production':production_model
+}
--- manage.py
+++ manage.py
@@ -1,14 +1,26 @@
-from flask import Flask
-from config import Config
+from flask import Flask, session
+from config import config
+from flask_sqlalchemy import SQLAlchemy
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+from flask_session import Session
+from flask_wtf import CSRFProtect
 
 app = Flask(__name__)
-app.config.from_object(Config)
+db = SQLAlchemy(app)
+manager = Manager(app)
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
+app.config.from_object(config['development'])
+CSRFProtect(app)
+Session(app)
 
 
 @app.route('/')
 def index():
+    session['name'] = 'python02'
     return 'hello world'
 
 if __name__ == '__main__':
     print(app.url_map)
-    app.run()
+    manager.run()
